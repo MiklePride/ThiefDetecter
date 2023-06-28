f44821c13dd946ff461b6c875ae61790d3a8a91a
Михаил Денисов <Migelius.invest@gmail.com>
2023-06-28 14:05:23 +0300

    Modify

--- Assets/Scripts/Alarm.cs
+++ Assets/Scripts/Alarm.cs
@@ -6,8 +6,7 @@ using UnityEngine;
 public class Alarm : MonoBehaviour
 {
     private AudioSource _alarmAudio;
-    private float _minVolume = 0f;
-    private float _maxVolume = 1.0f;
+    private float _targetVolume;
     private float _duration = 0.3f;
 
     private void Awake()
@@ -17,37 +16,27 @@ public class Alarm : MonoBehaviour
 
     public void TurnOnAlarm()
     {
-        StartCoroutine(ChangeValue());
+        _targetVolume = 1f;
+
+        StartCoroutine(ChangeValue(_targetVolume));
     }
 
     public void TurnOffAlarm()
     {
-        bool isPlaying = false;
+        _targetVolume = 0f;
 
-        StartCoroutine(ChangeValue(isPlaying));
+        StartCoroutine(ChangeValue(_targetVolume));
     }
 
-    private IEnumerator ChangeValue(bool isPlaying = true)
+    private IEnumerator ChangeValue(float targetVolume)
     {
-        if (isPlaying)
+        _alarmAudio.Play();
+
+        while (_alarmAudio.volume != targetVolume)
         {
-            _alarmAudio.Play();
+            _alarmAudio.volume = Mathf.MoveTowards(_alarmAudio.volume, targetVolume, _duration * Time.deltaTime);
 
-            while (_alarmAudio.volume < _maxVolume)
-            {
-                _alarmAudio.volume = Mathf.MoveTowards(_alarmAudio.volume, _maxVolume, _duration * Time.deltaTime);
-
-                yield return null;
-            }
-        }
-        else
-        {
-            while (_alarmAudio.volume > _minVolume)
-            {
-                _alarmAudio.volume = Mathf.MoveTowards(_alarmAudio.volume, _maxVolume, -_duration * Time.deltaTime);
-
-                yield return null;
-            }
+            yield return null;
         }
     }
 }
